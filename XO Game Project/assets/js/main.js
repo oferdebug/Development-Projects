c67505146f9ada2caf66542c19ffe35b0cd3e1aa
oferdebug <125661677+oferdebug@users.noreply.github.com>
2023-11-20 13:40:26 +0200

    FinalProject

--- XO Game Project/assets/js/main.js
+++ XO Game Project/assets/js/main.js
@@ -1,5 +1,5 @@
-var playerturn, moves, isGameOver, span, restartButton;
-playerturn = "x";
+var playerTurn, moves, isGameOver, span, restartButton;
+playerTurn = "x";
 moves = 0;
 isGameOver = false;
 span = document.getElementsByTagName("span");
@@ -8,8 +8,8 @@ restartButton =
 
 function play(y) {
   if (y.dataset.player == "none" && window.isGameOver == false) {
-    y.innerHTML = playerturn;
-    y.dataset.player = playerturn;
+    y.innerHTML = playerTurn;
+    y.dataset.player = playerTurn;
     moves++;
     if (playerTurn == "x") {
       playerTurn = "o";
@@ -18,7 +18,7 @@ function play(y) {
     }
   }
 
-  /* win Types */
+  /* Win Types */
 
   checkWinner(1, 2, 3);
   checkWinner(4, 5, 6);
@@ -29,54 +29,74 @@ function play(y) {
   checkWinner(1, 5, 9);
   checkWinner(3, 5, 7);
 
-  /*No Winner*/
+  /* No Winner */
 
   if (moves == 9 && isGameOver == false) {
     draw();
   }
+}
 
-  function checkWinner(a, b, c) {
-    a--;
-    b--;
-    c--;
-    if (
-      span[a].dataset.player === span[b].dataset.player &&
-      span[b].dataset.player === span[c].dataset.player &&
-      span[a].dataset.player === span[c].dataset.player &&
-      (span[a].dataset.player === "x" || span[a].dataset.player === "o") &&
-      isGameOver == false
-    ) {
-      span[a].perentNode.className += "activebox";
-      span[b].perentNode.className += "activebox";
-      span[c].perentNode.className += "activebox";
-      GameOver(a);
-    }
+function checkWinner(a, b, c) {
+  a--;
+  b--;
+  c--;
+  if (
+    span[a].dataset.player === span[b].dataset.player &&
+    span[b].dataset.player === span[c].dataset.player &&
+    span[a].dataset.player === span[c].dataset.player &&
+    (span[a].dataset.player === "x" || span[a].dataset.player === "o") &&
+    isGameOver == false
+  ) {
+    span[a].parentNode.className += " activeBox";
+    span[b].parentNode.className += " activeBox";
+    span[c].parentNode.className += " activeBox";
+    gameOver(a);
   }
+}
 
-  function playAgain() {
-    document
-      .getElementsByClassName("alert")[0]
-      .perentNode.removeChild(document.getElementsByClassName("alert")[0]);
-      resetGame();
-      window.isGameOver=false;
-      for (var k=0; k<span.length; k++) {
-        span[k].perentNode.className=span[k].perentNode.className.replace("activebox", "");
-      }
+function playAgain() {
+  document
+    .getElementsByClassName("alert")[0]
+    .parentNode.removeChild(document.getElementsByClassName("alert")[0]);
+  resetGame();
+  window.isGameOver = false;
+  for (var k = 0; k < span.length; k++) {
+    span[k].parentNode.className = span[k].parentNode.className.replace(
+      "activeBox",
+      ""
+    );
   }
+}
 
-  function resetGame() {
-    for(i=0; i< span.length; i++) {
-        span[i].dataset.player="none";
-        span[i].innerHTML = "&nbsp";
-    }
-    playerTurn="x";
+function resetGame() {
+  for (i = 0; i < span.length; i++) {
+    span[i].dataset.player = "none";
+    span[i].innerHTML = "&nbsp;";
   }
+  playerTurn = "x";
+}
 
-  function gameOver(a) {
-    var gameOverAlertElement="<b>GAME OVER </b><br><br> Player " + span[a].dataset.player.toUpperCase() + ' Win !!! <br><br>' + restartButton;
-    var div=document.createElement("div");
-    div.classname="alert";
-    div.innerHTML=gameOverAlertElement;
-    
-  }
+function gameOver(a) {
+  var gameOverAlertElement =
+    "<b>GAME OVER </b><br><br> Player " +
+    span[a].dataset.player.toUpperCase() +
+    " Win !!! <br><br>" +
+    restartButton;
+  span[a].dataset.player.toUpperCase() + " Win !!! <br><br>" + restartButton;
+  var div = document.createElement("div");
+  div.className = "alert";
+  div.innerHTML = gameOverAlertElement;
+  document.getElementsByTagName("body")[0].appendChild(div);
+  window.isGameOver = true;
+  moves = 0;
+}
+
+function draw() {
+  var drawAlertElement = "<b>DRAW!!!</b><br><br>" + restartButton;
+  var div = document.createElement("div");
+  div.className = "alert";
+  div.innerHTML = drawAlertElement;
+  document.getElementsByTagName("body")[0].appendChild(div);
+  window.isGameOver = true;
+  moves = 0;
 }
